243be287844fb785b4012b6be11322b55514a69a
aminya <aminyahyaabadi74@gmail.com>
2020-07-17 05:28:35 -0500

    using rollup-plugin-atomic

--- rollup.config.js
+++ rollup.config.js
@@ -1,66 +1,12 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-// import coffeescript from 'rollup-plugin-coffee-script';
-// import json from "@rollup/plugin-json"
-import { terser } from "rollup-plugin-terser";
-import autoExternal from 'rollup-plugin-auto-external';
+import { createPlugins, createConfig } from 'rollup-plugin-atomic'
 
-let plugins = [
-  autoExternal({
-    builtins: true,
-    dependencies: false,
-    peerDependencies: false
-  }),
+const plugins = createPlugins(['ts', 'js'], false) // babel is false
 
-  // so Rollup can convert TypeScript to JavaScript
-  typescript({
-    noEmitOnError: false,
-  }),
+const config = createConfig(
+	'src/main.ts',
+	'dist',
+	'cjs',
+	['atom'],
+	plugins)
 
-  // if any (in deps as well): Convert CoffeeScript to JavaScript
-  // coffeescript(),
-
-  // // so Rollup can bundle JSON to JavaScript
-  // json(
-  //   { compact: true }
-  // ),
-
-  // so Rollup can find externals
-  resolve({ extensions: ["ts", ".js", ".coffee"], preferBuiltins: true }),
-
-  // so Rollup can convert externals to an ES module
-  commonjs(),
-];
-
-// minify only in production mode
-if (process.env.NODE_ENV === "production") {
-  plugins.push(
-    // minify
-    terser({
-      ecma: 2018,
-      warnings: true,
-      compress: {
-        drop_console: false,
-      },
-    })
-  );
-}
-
-export default [
-  {
-    input: "src/main.ts",
-    output: [
-      {
-        dir: "dist",
-        format: "cjs",
-        sourcemap: true,
-      },
-    ],
-    // loaded externally
-    external: [
-      "atom",
-    ],
-    plugins: plugins,
-  },
-];
+export default config

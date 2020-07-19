ed771dfaf6415c7d0c4ab7160e7e8bf9913d3f1c
aminya <aminyahyaabadi74@gmail.com>
2020-07-19 04:45:14 -0500

    fix package-deps checker

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function package_deps() {
   // Add entries from package-deps here manually
   // (to prevent loading atom-package-deps and package.json when the deps are already loaded)
   const deps: string[] = [];
-  if (!deps.some((p) => atom.packages.isPackageLoaded(p))) {
+  if (deps.some((p) => !atom.packages.isPackageLoaded(p))) {
     await import("atom-package-deps").then((atom_package_deps) => {
       atom_package_deps.install("atom-ide-template");
     });
